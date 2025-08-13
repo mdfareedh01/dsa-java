c350166dd4157c44c6525eba5549d322f30caab9
Mohammed Fareedh <mdfareedh01@gmail.com>
2025-08-14 02:08:12 +0530

    Organized the Array classes

---
+++ array/Main.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Main {
+    public static void main(String[] args) {
+        Array Array = new Array(3);
+        Array.insert(10);
+        Array.insert(20);
+        Array.insert(30);
+        Array.insert(40);
+        Array.insert(50);
+        Array.print();
+        Array.removeAt(2);
+        Array.print();
+        System.out.println(Array.indexOf(40));        
+    }
+}
