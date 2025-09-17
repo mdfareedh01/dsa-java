4cb285dccc468786c14136c33437b010f49f928d
Mohammed Fareedh <mdfareedh01@gmail.com>
2025-09-18 02:09:31 +0530

    twosum practiced

---
+++ twosum.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class twosum {
+    // If nums = [1, 5, 3, 7] and target = 8
+    
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,5,3,7};
+        int target = 8;
+
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[i] + nums[j] == target) {
+                    System.out.println("["+i+ ","+ j+"]");
+                    break;
+                }
+            }
+        }
+
+        Map<Integer, Integer> seen = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if(seen.containsKey(complement)) {
+                System.out.println("["+seen.get(complement)+ ","+ i +"]");
+            }
+
+            seen.put(nums[i], i);
+        }
+
+        
+
+
+        
+    }
+}
