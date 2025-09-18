828c5887fdbacfdcd27862cb78e5461de7494c2f
Mohammed Fareedh <mdfareedh01@gmail.com>
2025-09-18 18:47:15 +0530

    Corrected twosum

--- twosum.java
+++ twosum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,27 +9,27 @@ public class twosum {
         int[] nums = new int[] {1,5,3,7};
         int target = 8;
 
-        for(int i = 0; i < nums.length; i++) {
-            for(int j = 0; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    System.out.println("["+i+ ","+ j+"]");
-                    break;
-                }
-            }
-        }
+        // for(int i = 0; i < nums.length; i++) {
+        //     for(int j = 0; j < nums.length; j++) {
+        //         if(nums[i] + nums[j] == target) {
+        //             System.out.println(Arrays.toString(new int[] { i, j }));
+        //             break;
+        //         }
+        //     }
+        // }
 
         Map<Integer, Integer> seen = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];
-
             if(seen.containsKey(complement)) {
-                System.out.println("["+seen.get(complement)+ ","+ i +"]");
+                System.out.println(Arrays.toString(new int[] {seen.get(complement),i}));
+                break;
             }
 
             seen.put(nums[i], i);
         }
 
-        
+
 
 
         
