25025ef2e59a40523b09211380b43bf968e03059
Mohammed Fareedh <mdfareedh01@gmail.com>
2025-09-20 15:53:40 +0530

    Contains Duplicate Problem 217

--- blind75.java
+++ blind75.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class blind75 {
     // If nums = [1, 5, 3, 7] and target = 8
@@ -12,7 +14,9 @@ public class blind75 {
         // twoSumUsingBruteForce();
         // twoSumUsingHasMap();
         // System.out.println(isAnagram("fareedh", "hdreraf"));
-        groupAnagram(new String[] {"eat","tea","tan","ate","nat","bat"});
+        // groupAnagram(new String[] {"eat","tea","tan","ate","nat","bat"});
+        // System.out.println(hasDuplicate(new int[] {1,2,3,4}));
+        System.out.println(hasDuplicateUsingHashSet(new int[] {1,2,3,4}));
     }
 
     public static boolean isAnagram(String s, String t) {
@@ -82,4 +86,32 @@ public class blind75 {
 
         System.out.println(new ArrayList<>(map.values()));
     }
+
+    public static boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap();
+
+        for(int i = 0; i < nums.length; i++) {
+            if(!map.containsKey(nums[i]))
+                map.put(nums[i], 1);
+            else {
+                map.put(nums[i], map.get(nums[i])+1);
+                return true;
+            }
+        }
+
+        return false;
+    } 
+
+    public static boolean hasDuplicateUsingHashSet(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for(int i: nums){
+            if(set.contains(i))
+                return true;
+            set.add(i);
+        }
+        return false;
+    }
+
+    
 }
